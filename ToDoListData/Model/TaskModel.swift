31f9346fc064c0b8dd587e015eb146dcd56fab09
Pedro Luis <pedroluisnr22@gmail.com>
2023-12-04 21:32:46 -0500

    feat: ✨ create UUID in TaskModel

--- ToDoListData/Model/TaskModel.swift
+++ ToDoListData/Model/TaskModel.swift
@@ -3,13 +3,13 @@ import SwiftData
 
 @Model
 final class TaskModel {
-    let uuid: UUID
+    var uuid: UUID
     var name: String
     var status: String
     var createdAt: Date
     
-    init(uuid: UUID, name: String, status: String, createdAt: Date) {
-        self.uuid = uuid
+    init(name: String, status: String, createdAt: Date) {
+        self.uuid = UUID()
         self.name = name
         self.status = status
         self.createdAt = createdAt
---
+++ ToDoListData/Tests/LocalStorageTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pedro Luis on 3/12/23.
+//
+
+import Foundation
