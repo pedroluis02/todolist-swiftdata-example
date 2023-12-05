de5caf11e704c043629c737178d49499ffda2ef4
Pedro Luis <pedroluisnr22@gmail.com>
2023-12-04 21:33:44 -0500

    feat: ✨ featch TaskModel by Id

--- ToDoListData/Dao/TaskDao.swift
+++ ToDoListData/Dao/TaskDao.swift
@@ -1,16 +1,24 @@
-import SwiftUI
+import Foundation
 import SwiftData
 
 class TaskDao {
     private let modelContext: ModelContext = LocalStorage.shared.modelContext
-
     
     func fecthAll() -> [TaskModel] {
-        let descriptor = FetchDescriptor<TaskModel>(sortBy: [SortDescriptor(\.name)])
+        let descriptor = FetchDescriptor<TaskModel>()
         return try! modelContext.fetch(descriptor)
     }
     
-    func insert(model: TaskModel) {
+    func fetchById(_ uuid: UUID) -> TaskModel? {
+        let descriptor = FetchDescriptor<TaskModel>(
+            predicate: #Predicate {
+            $0.uuid == uuid
+        })
+        
+        return try! modelContext.fetch<TaskModel>(descriptor).first
+    }
+    
+    func insert(_ model: TaskModel) {
         modelContext.insert(model)
     }
 }
