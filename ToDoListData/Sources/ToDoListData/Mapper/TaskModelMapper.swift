4c2aae099fced5b9ff8f27c63ff6e53b8e762bc2
Pedro Luis <pedroluisnr22@gmail.com>
2024-09-23 22:01:28 -0500

    refactor: :hammer: using ToDoTask model

--- ToDoListData/Sources/ToDoListData/Mapper/TaskModelMapper.swift
+++ ToDoListData/Sources/ToDoListData/Mapper/TaskModelMapper.swift
@@ -2,11 +2,11 @@ import ToDoListDomain
 import Foundation
 
 final class TaskModelMapper {
-    func toDomain(_ model: TaskModel) -> Task {
-        return Task(uuid: model.uuid, name: model.name, description: model.name, status: TaskStatus(rawValue: model.status) ?? TaskStatus.none, createdAt: model.createdAt)
+    func toDomain(_ model: TaskModel) -> ToDoTask {
+        return ToDoTask(name: model.name, id: model.uuid.uuidString, description: model.name, status: ToDoTaskStatus(rawValue: model.status) ?? ToDoTaskStatus.none, createdAt: model.createdAt)
     }
     
-    func toSave(_ model: Task) -> TaskModel {
-        return TaskModel(name: model.name, status: model.status.rawValue, createdAt: Date())
+    func toSave(_ model: ToDoTask) -> TaskModel {
+        return TaskModel(name: model.name, status: model.status.rawValue, createdAt: model.createdAt)
     }
 }
